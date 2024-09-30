ba26b63b497517c86728558012ea467e325e4269
Erika Pauwels <erika.pauwels@gmail.com>
2024-09-30 15:02:45 +0200

    Add dispatcher rules for codelists and public services

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -2,24 +2,37 @@ defmodule Dispatcher do
   use Matcher
   define_accept_types [
     html: [ "text/html", "application/xhtml+html" ],
-    json: [ "application/json", "application/vnd.api+json" ]
+    json: [ "application/json", "application/vnd.api+json" ],
+    any: [ "*/*" ]
   ]
 
-  @any %{}
-  @json %{ accept: %{ json: true } }
-  @html %{ accept: %{ html: true } }
+  define_layers [ :static, :web_page, :api_services, :not_found ]
 
-  define_layers [ :static, :services, :fall_back, :not_found ]
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule:
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-  #
-  # Run `docker-compose restart dispatcher` after updating
-  # this file.
+  ###############
+  # API SERVICES
+  ###############
+
+  get "/concepts/*path", %{ layer: :api_services, accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://resource/concepts/"
+  end
+
+  get "/concept-schemes/*path", %{ layer: :api_services, accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://resource/concept-schemes/"
+  end
+
+  match "/public-services/*path", %{ layer: :api_services, accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://resource/public-services/"
+  end
+
+  match "/websites/*path", %{ layer: :api_services, accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://resource/websites/"
+  end
+
+
+  #################
+  # NOT FOUND
+  #################
 
   match "/*_", %{ layer: :not_found } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
